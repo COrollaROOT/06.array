8c8cb5cfd7134dac20679e354da34c01aa59c6c0
COrollaROOT <doctor6749@gmail.com>
2025-03-21 15:37:16 +0900

    06.array
    배열 백업

---
+++ 배열/Program.cs
@@ -0,0 +1,115 @@
+﻿namespace 배열
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  int value; // 정수 저장소 value로 하나 만들기
+            //  value = 10; // 저장소에 10 저장하기
+            //
+            //  int[] values; // 정수 여러개 담을 수 있는 저장소 만들기
+            //  values = new int[10]; //10개짜리 저장소 만들기
+
+            //  int[] scores = new int[5];
+            //
+            //  scores[0] = 10; // 0번째 요소 저장
+            //  scores[1] = 20; // 1번째 요소 저장
+            //  scores[2] = 30; // 2번째 요소 저장
+            //  scores[3] = 40; // 3번째 요소 저장
+            //  scores[4] = 50; // 4번째 요소 저장
+            //
+            //  Console.WriteLine(scores[0]);    // 0번째 요소 저장
+            //  Console.WriteLine(scores[1]);    // 1번째 요소 저장
+            //  Console.WriteLine(scores[2]);    // 2번째 요소 저장
+            //  Console.WriteLine(scores[3]);    // 3번째 요소 저장
+            //  Console.WriteLine(scores[4]);    // 4번째 요소 저장
+            //  
+            //  Console.WriteLine("배열이 있는 갯수는 : {0}",scores.Length); // 배열.Length(길이) : 총 배열 갯수
+
+
+            //  string[] shortCuts = new string[5];
+
+            //  shortCuts[0] = "포션";
+            //  shortCuts[1] = "파폭";
+            //  shortCuts[2] = "부적";
+            //  shortCuts[3] = "폭탄";
+            //  shortCuts[4] = "연막";
+
+            //  for (int i = 0; i < shortCuts.Length; i++) // 4회 반복
+            //  {
+            //      Console.WriteLine("{0}번째 아이템은 {1} 입니다.", i + 1, shortCuts[i]);
+            //  }
+
+            //  foreach (string item in shortCuts)
+            //  {
+            //      Console.WriteLine("배열에 있는 아이템 : {0}", item);
+            //  }
+
+            // string[] students = new string[75];
+            // string[,] shortCuts = new string[3, 4];
+            //
+            // shortCuts[0, 0] = "대검";
+            // shortCuts[0, 1] = "태도";
+            // shortCuts[0, 2] = "한손검";
+            // shortCuts[0, 3] = "쌍검";
+            //
+            // shortCuts[1, 0] = "랜스";
+            // shortCuts[1, 1] = "건랜스";
+            // shortCuts[1, 2] = "해머";
+            // shortCuts[1, 3] = "수렵피리";
+            //
+            // shortCuts[2, 0] = "조충곤";
+            // shortCuts[2, 1] = "헤비보우건";
+            // shortCuts[2, 2] = "라이트보우건";
+            // shortCuts[2, 3] = "활";
+            //
+            // for (int i = 0; i < shortCuts.GetLength(0); i++)
+            // {
+            //     for (int j = 0; j < shortCuts.GetLength(1); j++)
+            //     {
+            //         Console.WriteLine("{0} 번째줄 {1} 번째 아이템 : {2}", i, j, shortCuts[i, j]);
+            //     }
+            //
+            //     
+            // }
+            //
+            // Console.WriteLine("총 아이템 갯수는 {0}",shortCuts.Length);
+
+            //     string text = "abcde";
+            //
+            //     char[] array = text.ToCharArray();
+            //
+            //     for (int i = 0; i < array.Length; i++)
+            //     {
+            //         Console.WriteLine(array[i]);
+            //     }
+            //     array[2] = 'x'; // 2번째 문자를 'x'로 바꿔줘
+            //
+            //     for (int i = 0; i < array.Length; i++)
+            //     {
+            //         Console.WriteLine(array[i]); // 문자 하나씩 출력
+            //     }
+            //
+            //     text = new string(array); // new : 저장소를 만들고 찾아 들어간다
+            //     Console.WriteLine(text); //문자열 전체를 출력
+
+
+            //   for (int i = 128; i >= 20; i/=2)
+            //   {
+            //       Console.Write(i);
+            //   }
+
+            
+         
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
